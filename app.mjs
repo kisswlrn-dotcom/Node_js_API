f0f77dc6b0715c0ad796cab9c74419b9ea5c1adc
jasonlee <kisswlrn@gmail.com>
2025-11-27 21:01:25 +0900

    프론트엔드 + Node.js API 연동 과제

---
+++ app.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import postsRouter from "./router/posts.mjs";
+import authRouter from "./router/auth.mjs";
+import { config } from "./config.mjs";
+import { connectDB } from "./db/database.mjs";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/post", postsRouter);
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+connectDB().then(() => {
+  app.listen(config.host.port);
+});
---
+++ router/auth.mjs
@@ -0,0 +1,37 @@
+import express from "express";
+import * as authController from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
+("../middleware/validator.mjs");
+import { isAuth } from "../middleware/auth.mjs";
+const router = express.Router();
+
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("아이디 최소 4자이상 입력")
+    .matches(/^[a-zA-Z0-9]+$/)
+    .withMessage("아이디 특수문자 사용불가"),
+  body("password")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("비밀번호 최소 4자이상 입력"),
+  validate,
+];
+
+const validateSignup = [
+  ...validateLogin,
+  body("email").trim().isEmail().withMessage("이메일 형식 확인"),
+  validate,
+];
+
+// 회원 가입
+router.post("/signup", validateSignup, authController.signup);
+
+// 로그인
+router.post("/login", validateLogin, authController.login);
+
+// 로그인 유지
+router.post("/me", isAuth, authController.me);
+export default router;
